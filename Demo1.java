c5eb3b019c8214a37575c9f59fe7bd4a16446048
piao2080 <76135481@qq.com>
2025-02-11 14:19:31 +0800

    测试pipeline-demo2-14：18

--- Demo1.java
+++ Demo1.java
@@ -1,9 +1,11 @@
 /**
- * test java
+ * 描述：
+ * 创建人：飘
+ * 创建时间：2025/2/8
  **/
 public class Demo1 {
     public static void main(String[] args) {
 
-        System.out.println("hello 111@@231");
+        System.out.println("hello world 沈阳！");
     }
 }
